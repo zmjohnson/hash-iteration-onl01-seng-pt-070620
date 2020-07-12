8fee81b9c4249c1f9206db75c9653f2966c1305f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-12 19:49:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
+it "only prints the birthday greeting if the birthday kid 12 or younger" do
+    birthday_kids = {
+      "Timmy" => 9,
+      "Sarah" => 6,
+      "Amanda" => 27
+    }
+    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
+  end
+end
