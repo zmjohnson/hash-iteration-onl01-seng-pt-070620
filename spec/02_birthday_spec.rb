f1f55b749592f755379d97dbb6c893f38678b78c
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-12 20:04:03 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -13,17 +13,3 @@ describe "#happy_birthday" do
   end
 end
 
-## BONUS ##
-## Change "xit" to "it" in the test block to enable
-describe "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" do
-    birthday_kids = {
-      "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27
-    }
-
-    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
-
-  end
-end
