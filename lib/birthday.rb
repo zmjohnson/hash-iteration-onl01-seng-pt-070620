d4b3d87ff1c5f566770afd776deece0f22f90061
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-12 20:02:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,16 +5,22 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+  # add your code snippet here!
+ birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+ end
+
+
+
 end
 
 
-def age_appropriate_birthday(birthday_kids)
+
+def age_appropriate_birthday (birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age<=12 then puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else puts "You are too old for this."
-  end
-end
+
+    end 
+  end 
 end
