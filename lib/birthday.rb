2aac22e80728ebc54386f83743266a78fb14cabe
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-12 19:51:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,9 @@
 #   "Amanda" => 27
 # }
 
-def birthday_kids.each do |kids_name, age|
+def happy_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
